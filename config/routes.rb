891fb76d659c8145e8b1cdcfc988add358d3ea7f
Juan Villalobos <juan.villalobos1992@gmail.com>
2015-12-08 17:03:30 -0800

    Added root to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :appointments
   resources :barbers
   devise_for :users
+
+  root 'barbers#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
